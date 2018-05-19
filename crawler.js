728acfd2ce9974e0a257c6b30d10dbb477f68be0
Anish <anish@doctual.com>
2018-05-19 14:05:51 +0300

    crawler multiple users

--- crawler.js
+++ crawler.js
@@ -1,56 +1,79 @@
-const Crawler = require("simplecrawler");
-const cheerio = require('cheerio');
-const buffer = require('buffer');
+var Crawler = require("simplecrawler");
+const fs = require('fs');
 
-const crawler = new Crawler('https://soundgasm.net/u/The_Lady_Aurora/');
+function matcher(str, regex) {
+    const matched = str.match(regex);
+    if (matched === null) {
+        return [];
+    }
+    return matched;
+}
 
 const data = [];
+const crawlerInstances = [];
 
-// crawler.maxDepth = 3;
-crawler.interval = 0; // Ten seconds
-crawler.maxConcurrency = 2;
-
-crawler.discoverResources = function(buffer, queueItem) {
-    var $ = cheerio.load(buffer.toString("utf8"));
-    return $("a[href]").map(function () {
-        return $(this).attr("href");
-    }).get();
-};
-
-crawler.addFetchCondition(function(queueItem, referrerQueueItem, callback) {
-    // We only ever want to move one step away from example.com, so if the
-    // referrer queue item reports a different domain, don't proceed
-    callback(null, queueItem.url.match(/^https:\/\/soundgasm\.net\/u\/The_Lady_Aurora\//gi));
-});
-
-crawler.on('queueadd', (queueItem, referrerQueueItem) => {
-    console.log(queueItem);
-});
-
-crawler.on('fetchcomplete', (queueItem, responseBuffer, response) => {
-    (function (queueItem, responseBuffer, response) {
-        setTimeout(() => {
-            const webpage = responseBuffer.toString("utf8");
-            const $ = cheerio.load(webpage);
-            console.log(webpage);
-            
-            let title = $('.sound-details>a').textContent;
-            let uploader = queueItem.path.match(/\/u\/(.*)\/?/)
-            let downloadURL = webpage.match(/(https:\/\/soundgasm.net\/sound.*)"/)[1];
-            let sound = {
-                title: title,
-                uploader: uploader,
-                downloadURL: downloadURL
-            };
-            data.push(sound);
-        }, 2000);
-    }(queueItem, responseBuffer, response))
-        
-    
-});
-
-crawler.on('complete', () => {
-    console.log(data);
+const urls = [
+    'https://soundgasm.net/u/The_Lady_Aurora',
+    'https://soundgasm.net/u/kinkyshibby'
+]
+urls.forEach((url) => {
+    const crawler = new Crawler(url);
+    crawlerInstances.push(crawler);
 })
 
-crawler.start();
+function run(crawlers) {
+    if (!crawlers.length) {
+        console.log(data);
+        fs.writeFileSync('data.json', JSON.stringify(data));
+        return;
+    }
+    (function (crawler) {
+        scrape(crawler);
+        crawler.on('complete', function(){
+            run(crawlers);
+        });
+    }(crawlers.shift()))
+}
+
+function scrape(crawler) {
+    crawler.interval = 0; // Ten seconds
+    crawler.maxConcurrency = 2;
+
+    crawler.discoverResources = function(buffer, queueItem) {
+        var $ = cheerio.load(buffer.toString("utf8"));
+        return $("a[href]").map(function () {
+            return $(this).attr("href");
+        }).get();
+    };
+
+    crawler.addFetchCondition(function(queueItem, referrerQueueItem, callback) {
+        callback(null, queueItem.url.match(/^https:\/\/soundgasm\.net\/u\/(.*)\//gi));
+    });
+
+    crawler.on('fetchcomplete', function (queueItem, responseBuffer, response){
+        console.log('FETCH COMPLETE:: ', queueItem.url);
+        const _webpage = responseBuffer.toString("utf8");
+        let title = matcher(_webpage, /aria-label="title">(.*)</)[1];
+        let uploader = matcher(queueItem.url, /\/u\/(.*)\//)[1];
+        let downloadUrl = matcher(_webpage, /(https:\/\/soundgasm.net\/sound.*)"/)[1];
+
+        if (title && downloadUrl && uploader) {
+            const sound = {
+                title: title,
+                uploader: uploader,
+                downloadURL: downloadUrl
+            };
+            data.push(sound);
+        }
+                       
+        
+    });    
+    crawler.on('queueadd', function (queueItem, referrerQueueItem){
+        console.log(queueItem.url);
+    });
+    
+    crawler.start(); 
+}
+
+
+run(crawlerInstances);
